6dafb6ad03e33cd9aae47fd8c920c3d8d5e1fbf0
Shubham Kumar <ishubhamjnv@gmail.com>
2024-01-02 13:29:37 +0530

    Vite React Basics

---
+++ 02vitereactbasics/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Heading from "./Heading";
+
+function App() {
+  const mainHeading = "JavaScript";
+  return (
+    <div>
+      <Heading />
+      <h1>Test Heading </h1>
+      <p>{mainHeading}</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ 02vitereactbasics/src/main.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App | React</h1>
+    </div>
+  );
+}
+
+// const ReactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   children: "Click me to visit google",
+// };
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">
+    Visit google
+  </a>
+);
+
+const reactElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "click me to visit google",
+  anotherElement
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(reactElement);
